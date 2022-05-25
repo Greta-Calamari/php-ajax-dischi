1be44163c5872ce649238cc00319ed0b3e9b20a0
ladykakki <greta_calamari@hotmail.it>
2022-05-25 17:18:19 +0200

    ..

--- script/script.js
+++ script/script.js
@@ -4,24 +4,31 @@ const app = new Vue({
     dischi: [],
     categories: [],
     selectedCategory: "",
+    apiPath: "./server.php",
   },
   methods: {
-    getData() {
-      axios.get("server.php").then((res) => {
+    getData(genre = null) {
+      let path = this.apiPath;
+      if (genre) {
+        path = this.apiPath + "?genre=" + genre;
+      }
+      axios.get(path).then((res) => {
         //   console.log(res);
         this.dischi = res.data;
         //   console.log(this.dischi);
-        this.dischi.forEach((disco) => {
-          if (!this.categories.includes(disco.genre)) {
-            this.categories.push(disco.genre);
-          }
-        });
+        if (this.categories.length < 1) {
+          this.dischi.forEach((disco) => {
+            if (!this.categories.includes(disco.genre)) {
+              this.categories.push(disco.genre);
+            }
+          });
+        }
       });
     },
 
     search() {
-      console.log(this.selectedCategory);
-      //   this.getData();
+      //   console.log(this.selectedCategory);
+      this.getData(this.selectedCategory);
     },
   },
   mounted() {
