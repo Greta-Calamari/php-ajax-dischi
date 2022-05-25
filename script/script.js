3eb7e76b989b7a1df3ef7cb8d6952324b6914e65
ladykakki <greta_calamari@hotmail.it>
2022-05-25 16:29:41 +0200

    creando filtraggio

--- script/script.js
+++ script/script.js
@@ -2,13 +2,19 @@ const app = new Vue({
   el: "#app",
   data: {
     dischi: [],
+    categories: [],
   },
   methods: {},
   mounted() {
     axios.get("server.php").then((res) => {
-      console.log(res);
+      //   console.log(res);
       this.dischi = res.data;
-      console.log(this.dischi);
+      //   console.log(this.dischi);
+      this.dischi.forEach((disco) => {
+        if (!this.categories.includes(disco.genre)) {
+          this.categories.push(disco.genre);
+        }
+      });
     });
   },
 });
