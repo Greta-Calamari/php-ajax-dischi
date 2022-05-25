bec048f5e9bbc6124f021086ea233addea27c373
ladykakki <greta_calamari@hotmail.it>
2022-05-25 17:18:10 +0200

    errore lato front e errore lato back fixati

--- server.php
+++ server.php
@@ -2,10 +2,22 @@
 <?php
 
 include __DIR__.'/data.php';
+$risultato = [];
+
+if(isset($_GET['genre'])){
+    foreach($dischi as $disco){
+        if($disco['genre'] == $_GET['genre']){
+            $risultato[]=$disco;
+        }
+    }
+
+}else{
+    $risultato = $dischi;
+}
 
 header('Content-Type: application/json');
 
-echo json_encode($dischi);
+echo json_encode($risultato);
 
 
 
